9c5202fb38fbddc2dc77da9d73fcc70369792747
DeDuShKa12 <pasharudiuk97@gmail.com>
2023-04-20 18:50:30 +0300

    HW2 done

---
+++ src/users/dto/users.dto.ts
@@ -0,0 +1,60 @@
+import {
+  IsBoolean,
+  IsEmail,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class createUserDto {
+  @ApiProperty({ required: true })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty({ required: false })
+  @IsNumber()
+  @IsOptional()
+  age: number;
+
+  @ApiProperty({ required: true, example: 'user@gmail.com' })
+  @IsString()
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @ApiProperty({ required: true })
+  @IsString()
+  @IsNotEmpty()
+  city: string;
+
+  @ApiProperty({ required: false })
+  @IsBoolean()
+  @IsOptional()
+  status: boolean;
+}
+
+export class updateUserDto {
+  @IsString()
+  @IsOptional()
+  name: string;
+
+  @IsNumber()
+  @IsOptional()
+  age: number;
+
+  @IsString()
+  @IsEmail()
+  @IsOptional()
+  email: string;
+
+  @IsString()
+  @IsOptional()
+  city: string;
+
+  @IsBoolean()
+  @IsOptional()
+  status: boolean;
+}
